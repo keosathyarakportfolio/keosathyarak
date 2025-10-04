5e16c149d833c5d481f96e19d82a2b736da46beb
KEO SATHYARAK <keosathyarak@gmail.com>
2025-10-04 22:56:48 +0700

    Fix API with onrender

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -13,7 +13,7 @@ class DownloadController extends Controller
     public function __construct()
     {
         // Read from environment variable
-        $this->pythonApi = env('PYTHON_API', 'https://backend-taupe-zeta-28.vercel.app');
+        $this->pythonApi = env('PYTHON_API', 'https://downloader-ttg5.onrender.com');
     }
 
     public function showForm()
